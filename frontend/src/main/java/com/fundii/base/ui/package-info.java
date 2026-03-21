4268426557e7bb8a5362cf09437e3d0f4c5e8efb
surakiat-y <surakiat-y@live.ku.th>
2026-03-21 18:16:24 +0700

    Build Vandiin into frontend

---
+++ frontend/src/main/java/com/fundii/base/ui/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package com.fundii.base.ui;
+
+import org.jspecify.annotations.NullMarked;
---
+++ frontend/src/main/java/com/fundii/examplefeature/package-info.java
@@ -0,0 +1,5 @@
+@NullMarked
+package com.fundii.examplefeature;
+// TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
